89b9147bfb44b0a2f913e8ccfeb6fb585a12e6ff
tandisunarto <tandi.sunarto@hotmail.com>
2017-08-15 15:22:57 -0400

    create 2 tasks

--- observable-1/main.js
+++ observable-1/main.js
@@ -2,22 +2,28 @@ var task = require('./task');
 var observableTask = require('./observable-task')
 var services = require('./observer-services');
 
-var myTask = new observableTask(
-    {
-        name: 'create a demo for constructors',
-        user: 'john'
-    });
+var task_1 = new observableTask({
+    name: 'create a demo for constructors',
+    user: 'John'
+});
+
+var task_2 = new observableTask({
+    name: 'preparing material for next demo',
+    user: 'Jane'
+});
 
 var notificationSvc = new services.notificationService();
 var loggingSvc = new services.loggingService();
 var auditingSvc = new services.auditingService();
 
-myTask.addObserver(notificationSvc.update);
-myTask.addObserver(loggingSvc.update);
-myTask.addObserver(auditingSvc.update);
+task_1.addObserver(notificationSvc.update);
+task_1.addObserver(loggingSvc.update);
+task_1.addObserver(auditingSvc.update);
 
-myTask.save();
+task_1.save();
 
-// myTask.removeObserver(auditingSvc.update);
+console.log('-------------------');
 
-// myTask.save();
+task_2.addObserver(loggingSvc.update);
+
+task_2.save();
